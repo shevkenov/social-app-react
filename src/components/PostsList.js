1c38aba4b05dfa1a92bcfe723ff3998d83c6f03c
Shevkenov Andrey <shevkenov@gmail.com>
2021-06-14 17:31:44 +0300

    feat: creat postlist and postitem component

--- src/components/PostsList.js
+++ src/components/PostsList.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { ListGroup } from "react-bootstrap";
+import PostItem from "./PostItem";
+import Spinner from "./Spinner";
 
-export default function PostsList({posts}) {
+export default function PostsList({posts, isSuccess, isLoading, no_reply_tag}) {
+  if(isLoading) return <Spinner />
 
+  console.log(posts)
   return (
     <ListGroup variant="flush" className="border-bottom">
-      <div className="message">No posts for you right now</div>
+      {
+        isSuccess ? posts.map(post => (<PostItem  post={post} key={post._id} no_reply_tag={no_reply_tag}/>)) : 
+        <div className="message">No posts for you right now</div>}
     </ListGroup>
   );
 }
