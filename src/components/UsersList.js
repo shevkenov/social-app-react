bde71b9149410c9510fb610183edb1fc65e443b5
Shevkenov Andrey <shevkenov@gmail.com>
2021-06-16 20:34:51 +0300

    feat: userrs item features

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { ListGroup } from "react-bootstrap";
+import Spinner from "./Spinner";
+import UserItem from "./UserItem";
 
 export default function UsersList(props) {
-  const { className } = props;
+  const { className, isLoading, isSuccess, users, length, ...rest } = props;
+
+  if(isLoading) return <Spinner />
 
   return (
     <ListGroup className={`border-bottom ${className}`} variant="flush">
-      <div className="message font-weight-bold">No users to show</div>
+      {isSuccess ? users.slice(0, length).map(user => <UserItem user={user} key={user._id} {...rest}/>) :
+        <div className="message font-weight-bold">No users to show</div>}
     </ListGroup>
   );
 }
